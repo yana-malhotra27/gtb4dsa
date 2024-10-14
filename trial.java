93b82cb6710554405ec2cad4a917f99dd87ff2c0
yana-malhotra27 <yana27.ym@gmail.com>
2024-10-14 17:05:41 +0530

    first

---
+++ trial.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class trial {
+    /* */
+    public static void main(String[] args) {
+        // System.out.println("hello codegeeks");
+        // System.out.print("helllo");
+        Scanner sc=new Scanner(System.in);
+        //int n=sc.nextInt();
+        // String s=sc.nextLine();
+        // String m=sc.next();
+        // System.out.println(s+" "+m);
+        //int
+        //float
+        //double
+        //long
+        //short
+        //char
+        //String
+        //types of operators
+        //arithmetic operators
+        //+,-,/,*,%,++,--
+        //comparison operators
+        //>,<,<=,>=,==
+        //logical operators
+        // &&,||,!
+        // //assignment operator
+        // =,*=
+        // //bitwise operators
+        // >>,<<,|,^,&,~
+        int n=10;
+        if(n>5){
+            System.out.println("greater than 5");
+        }
+        else{
+            System.out.println("less");
+        }
+    }
+}
